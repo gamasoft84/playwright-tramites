035ed78ede253b093e6c3ee50f04eb8a1fbf02e0
gamasoft84 <richardgama@yahoo.com.mx>
2026-03-23 18:28:52 -0700

    updated environment config

--- ambientes.mjs
+++ ambientes.mjs
@@ -8,11 +8,18 @@ export const AMBIENTES = {
   UAT_ULTRASIST: {
     baseURL: 'https://front.v30.ultrasist.net',
     tramitesFile: 'tramites_UAT_ULTRASIST.json',
+    baseURLFull: 'https://front.v30.ultrasist.net/seleccion-tramite'
   },
   UAT_SAT: {
-    baseURL: 'https://wwwdev.ventanillaunica.gob.mx',
+    baseURL: 'http://10.218.140.246:8080',
     tramitesFile: 'tramites_UAT_SAT.json',
+    baseURLFull: 'http://10.218.140.246:8080/seleccion-tramite'
   },
+  DEV_SAT: {
+    baseURL: 'https://wwwdev.ventanillaunica.gob.mx',
+    tramitesFile: 'tramites_DEV_SAT.json',
+    baseURLFull: 'https://wwwdev.ventanillaunica.gob.mx/seleccion-tramite'
+  }
 };
 
 const KEYS = new Set(Object.keys(AMBIENTES));
